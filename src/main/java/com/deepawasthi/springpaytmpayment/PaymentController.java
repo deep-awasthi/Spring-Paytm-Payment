845ccf1cbe27c385e7cfdf0b95963eb1ec793c39
Deep Awasthi <da.madskull@gmail.com>
2023-03-22 19:11:34 +0530

    adding styles

--- src/main/java/com/deepawasthi/springpaytmpayment/PaymentController.java
+++ src/main/java/com/deepawasthi/springpaytmpayment/PaymentController.java
@@ -1,13 +1,21 @@
 package com.deepawasthi.springpaytmpayment;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PaymentController {
 
+
+    @Autowired
+    private PaytmDetails PaytmDetails;
     @GetMapping("/")
     public String home(){
         return "home";
+    }
+
+    public String pageRedirect(){
+        
     }
 }
