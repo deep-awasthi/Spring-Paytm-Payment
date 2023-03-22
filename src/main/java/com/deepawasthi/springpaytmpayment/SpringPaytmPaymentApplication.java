894aa080571a199de68960b0037278f4c8bbb846
Deep Awasthi <da.madskull@gmail.com>
2023-03-22 18:51:40 +0530

    Initial Commit

---
+++ src/main/java/com/deepawasthi/springpaytmpayment/SpringPaytmPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.deepawasthi.springpaytmpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaytmPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPaytmPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deepawasthi/springpaytmpayment/SpringPaytmPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepawasthi.springpaytmpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPaytmPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
