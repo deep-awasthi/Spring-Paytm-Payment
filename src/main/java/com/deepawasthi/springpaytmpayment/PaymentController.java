0f4270c212f64518542b082aec048da562c48620
Deep Awasthi <da.madskull@gmail.com>
2023-03-22 19:03:52 +0530

    adding home.html

--- src/main/java/com/deepawasthi/springpaytmpayment/PaymentController.java
+++ src/main/java/com/deepawasthi/springpaytmpayment/PaymentController.java
@@ -1,4 +1,13 @@
 package com.deepawasthi.springpaytmpayment;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PaymentController {
+
+    @GetMapping("/")
+    public String home(){
+        return "home";
+    }
 }
